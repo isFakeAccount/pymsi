ab19e54505be1f171cb5093f43047db37a79c94a
Yoshikage Kira <8bit_yoshikage_kira@proton.me>
2026-01-02 00:00:45 -0500

    Convert relative imports to abs and add missing __init__.py file
    - Convert relative imports to abs and add missing __init__.py file

--- src/pymsi/__init__.py
+++ src/pymsi/__init__.py
@@ -4,5 +4,5 @@ except ModuleNotFoundError:
     __version__ = ""
     __version_tuple__ = ()
 
-from .msi import *  # noqa: F403
-from .package import Package  # noqa: F401
+from pymsi.msi import *  # noqa: F403
+from pymsi.package import Package  # noqa: F401
